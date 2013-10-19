45c67e1155878b05a5d91d57d92dce5cfa887831
Jorge Yau <codenameyau@gmail.com>
2013-10-19 19:46:02 -0400

    Enabling logout

--- technetium/technetium/bitbucket/views.py
+++ technetium/technetium/bitbucket/views.py
@@ -1,11 +1,12 @@
 """
 Views for bitbucket application
 """
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib import auth
 from social.backends.bitbucket import BitbucketOAuth
 
 # Home page view
@@ -13,7 +14,7 @@ def home(request):
     if request.user.is_authenticated():
         return redirect('/done')
 
-    return render('home.html', {
+    return render(request, 'home.html', {
       'key': getattr(settings, 'SOCIAL_AUTH_BITBUCKET_KEY', None)
     })
 
@@ -24,3 +25,9 @@ def done(request):
       'user': request.user,
       'key': getattr(settings, 'SOCIAL_AUTH_BITBUCKET_KEY', None)
     })
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
--- technetium/technetium/urls.py
+++ technetium/technetium/urls.py
@@ -8,10 +8,12 @@ from bitbucket import views as views_bitbucket
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views_bitbucket.home),
 
+    # Technetium: bitbucket app
+    url(r'^$', views_bitbucket.home),
     url(r'^login/$', views_bitbucket.home),
     url(r'^done/$', views_bitbucket.done, name='done'),
+    url(r'^logout/$', views_bitbucket.logout),
 
     # Social Auth
     url(r'', include('social.apps.django_app.urls', namespace='social'))
