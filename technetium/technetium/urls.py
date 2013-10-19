4e789484d7f6853c142d9ca7a292dc64cd048012
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-19 14:43:54 -0400

    Fixing urls

--- technetium/technetium/urls.py
+++ technetium/technetium/urls.py
@@ -1,22 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'example.app.views.home'),
-    url(r'^login/$', 'example.app.views.home'),
-    url(r'^done/$', 'example.app.views.done', name='done'),
+    url(r'^$', 'technetium.views.home'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'technetium.views.home'),
+    url(r'^done/$', 'technetium.views.done', name='done'),
     url(r'', include('social.apps.django_app.urls', namespace='social'))
-    # Examples:
-    # url(r'^$', 'technetium.views.home', name='home'),
-    # url(r'^technetium/', include('technetium.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
