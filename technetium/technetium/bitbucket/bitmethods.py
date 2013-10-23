a5ab9938affc14c8c7cf8650c6ebba001bcccbe6
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-22 21:19:21 -0400

    Added bitmethods module

---
+++ technetium/technetium/bitbucket/bitmethods.py
@@ -0,0 +1,21 @@
+"""
+Module for common methods.
+
+Proposed methods:
+    - Method for getting a list of all repositories
+      belonging to the user.
+"""
+
+def get_repositories():
+    """
+    Gets all repositories that the user owns/has 
+    access privledges to.
+
+    Parameters:
+    -----------
+
+    Returns:
+    --------
+    """
+
+    pass
