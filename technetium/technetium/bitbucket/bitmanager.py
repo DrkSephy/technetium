af6b6a4353219a74c5321850a0a74a8770eb03a8
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-22 21:01:00 -0400

    Intermediate work: updated bitmanager module

--- technetium/technetium/bitbucket/bitmanager.py
+++ technetium/technetium/bitbucket/bitmanager.py
@@ -1,3 +1,26 @@
 """
 Module for managing various Bitbucket endpoints.
+
+Handles the following:
+
+    - Allows method to add repositories.
+    - Allows method to remove repositories.
 """
+
+import requests
+
+def add_repository():
+    """
+    Adds a repository to a list which is `followed`
+    by the user.
+
+    Parameters:
+    -----------
+
+
+    Returns:
+    --------
+
+    """
+    pass
+
