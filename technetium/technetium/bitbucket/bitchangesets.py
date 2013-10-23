7ea81e5988f3e4f81f3b2182fe19300f5e1a55b5
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-22 20:47:05 -0400

    Intermediate work on bitchangesets module

--- technetium/technetium/bitbucket/bitchangesets.py
+++ technetium/technetium/bitbucket/bitchangesets.py
@@ -1,3 +1,31 @@
 """
 Module for Bitbucket changesets aggregation.
+
+Proposed methods:
+    - GETs changesets for a given repository
+    - GETs changesets for multiple repositories.
+    - Parses returned JSON.
 """
+
+import requests
+
+
+def get_changesets():
+    """
+    Obtains a JSON dictionary of changesets across
+    a repository/repositories.
+
+    Parameters:
+    ----------
+
+
+    Returns:
+    -------
+    changesets: dictionary
+        - A dictionary containing [key][values] representing
+          all commits for the requested repositories.
+    """
+    pass
+
+def parse_changesets():
+    pass
