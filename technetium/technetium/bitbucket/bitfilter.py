04e741518b5a37846145f1a5acc49b85e24c9a55
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-24 18:11:37 -0400

    Intermediate work on bitfilter module

--- technetium/technetium/bitbucket/bitfilter.py
+++ technetium/technetium/bitbucket/bitfilter.py
@@ -1,4 +1,32 @@
 """
 Module for filtering various Bitbucket data.
+
+Handles:
+    - Filtering changesets by time/user/date.
+    - Filtering issues by type.
+    - Filtering issues by priority.
+    - Filtering issues by user.
+    - Filtering issues by date.
+    - Filtering issues by status.
 """
+
+def filter_issues_by_type():
+    """
+    Filters issues by type. (Blocker, Status, etc)
+    """
+    pass
+
+
+def filter_issues_by_priority():
+    """
+    Filters issues based on priority.
+    """
+    pass
+
+
+def filter_issues_by_user():
+    """
+    Filters issues based on users.
+    """
+    pass
 
