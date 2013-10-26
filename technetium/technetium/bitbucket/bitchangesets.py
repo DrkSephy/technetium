2be2645e9aaa4611d78dc6073a24a12d83cd206d
David Leonard <sephirothcloud1025@yahoo.com>
2013-10-26 13:53:05 -0400

    Updating Changesets module

--- technetium/technetium/bitbucket/bitchangesets.py
+++ technetium/technetium/bitbucket/bitchangesets.py
@@ -5,6 +5,26 @@ Proposed methods:
     - GETs changesets for a given repository
     - GETs changesets for multiple repositories.
     - Parses returned JSON.
+
+Bitbucket API requests start at the following endpoint:
+
+https://bitbucket.org/api/1.0/repositories/
+and have the following layout:
+
+https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/{endpoint}
+
+    - {accountname} : The Bitbucket User name
+    - {repo_slug} : The repository name 
+    - {endpoint} : The resource to request
+
+The calls also take the following extra query parameters:
+
+    - start: The hash value which the query starts from. The 
+             default start point is the most recent entry to
+             the earliest.
+
+    - limit: Integer value which represents the number of changesets
+             to return. 
 """
 
 import requests
@@ -14,6 +34,8 @@ def get_changesets():
     """
     Obtains a JSON dictionary of changesets across
     a repository/repositories.
+
+
 
     Parameters:
     ----------
