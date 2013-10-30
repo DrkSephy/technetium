2c23ab8c8ecb37fa28dd34b4f2c5b1d250faaa6e
Jorge Yau <codenameyau@gmail.com>
2013-10-29 23:34:19 -0400

    Adding get auth tokens

--- technetium/technetium/bitbucket/bitauth.py
+++ technetium/technetium/bitbucket/bitauth.py
@@ -1,8 +1,9 @@
 """
 Module for Bitbucket authentication and authorization.
 """
+from django.conf.settings import BITBUCKET_CONSUMER_KEY, BITBUCKET_CONSUMER_SECRET
 from social_auth.models import UserSocialAuth
-
+from requests_oauthlib import OAuth1
 
 def get_social_auth_data(request):
     """
@@ -17,3 +18,22 @@ def get_social_auth_data(request):
     """
     return UserSocialAuth.objects.get(user=request.user).extra_data
 
+
+def get_auth_tokens(extra_data):
+    """
+    Returns the authorization tokens required to access
+    protected resources from Bitbucket's API. Use this
+    function in the 'auth' parameter for requests.
+
+    Parameters:
+        - extra_data: Dictionary (from get_social_auth_data)
+
+    Returns: OAuth1 (object)
+    """
+    tokens = extra_data['access_token']
+    resource_owner_key = tokens['oauth_token']
+    resource_owner_secret = tokens['oauth_token_secret']
+    return OAuth1(BITBUCKET_CONSUMER_KEY,
+                  client_secret=BITBUCKET_CONSUMER_SECRET,
+                  resource_owner_key=resource_owner_key,
+                  resource_owner_secret=resource_owner_secret)
