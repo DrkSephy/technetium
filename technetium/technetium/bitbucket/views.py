0050435376ccb95e4b162582377da68b5196b616
David Leonard <dleonar00@ccny.cuny.edu>
2013-11-03 16:15:01 -0500

    Merged achieu00/technetium-teamdev into default

--- technetium/technetium/bitbucket/views.py
+++ technetium/technetium/bitbucket/views.py
@@ -48,7 +48,7 @@ def dashboard(request):
     auth_tokens = bitauth.get_auth_tokens(auth_data)
 
     # Render the last 5 issues
-    data['issues_json'] = bitissues.parse_issues(
+    data['issues_json'] = bitissues.parse_issues(request,
         bitissues.get_issues(user, repo, auth_tokens, 5))
 
     # Render the last 5 changesets
@@ -68,6 +68,14 @@ def dashboard_issues(request):
     repo = 'technetium'
     data = {}
 
+    limit = 13
+    filterNameValues={}
+    for n, v in request.GET.iteritems():
+        filterNameValues[n] = v
+        limit += 50
+
+    data['filterNameValues'] = filterNameValues
+
     # Get OAuth tokens
     auth_data = bitauth.get_social_auth_data(request.user)
     auth_tokens = bitauth.get_auth_tokens(auth_data)
@@ -76,8 +84,9 @@ def dashboard_issues(request):
     data['first_name'] = auth_data['first_name']
     data['last_name'] = auth_data['last_name']
     data['email'] = auth_data['email']
-    data['issues_json'] = bitissues.parse_issues(
-        bitissues.get_issues(user, repo, auth_tokens, 13))
+    data['issues_json'] = bitissues.parse_issues(request,
+        bitissues.get_issues(user, repo, auth_tokens, limit))
+
     return render(request, 'dashboard_issues.html', data)
 
 
