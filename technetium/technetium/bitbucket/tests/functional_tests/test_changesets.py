4c27353c595cbde0b6bedf49b0aa409c73647744
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-02 22:00:18 -0400

    Created functional test directory, started changeset test skeleton

---
+++ technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
@@ -0,0 +1,12 @@
+"""
+Functional tests for changesets module.
+"""
+
+import simplejson as json
+import requests
+import unittest
+import technetium.bitbucket.bitchangesets as changesets
+
+class BitchangesetsTests(unittest.TestCase):
+    pass
+
