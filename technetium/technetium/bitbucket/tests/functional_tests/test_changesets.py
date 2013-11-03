d75018f107c0633301ddec79d47e180f1e4ce46e
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-02 22:08:41 -0400

    Intermediate work on functional tests

--- technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
+++ technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
@@ -8,11 +8,21 @@ import unittest
 import technetium.bitbucket.bitchangesets as changesets
 
 class BitchangesetsTests(unittest.TestCase):
+
     
-    def test_get_changesets(self):
+    def test_get_public_changesets(self):
         """
         Tests that a public repository query has a 200 status code.
         """
         req_url = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/smw-koopa-krisis/issues/?limit=2')
         self.assertEqual(req_url.status_code, 200)
+
+    def test_get_private_changesets(self):
+        """
+        Tests that a private repository query without credentials will fail.
+        """
+        req_url = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/techetium/issues/?limit=2')
+        self.assertEqual(req_url.status_code, 404)
+
+
 
