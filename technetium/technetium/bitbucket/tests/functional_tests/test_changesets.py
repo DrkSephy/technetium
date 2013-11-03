35adb773291bc230c325e37093ffdf96391960dd
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-02 22:24:02 -0400

    Adding setup method

--- technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
+++ technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
@@ -7,22 +7,35 @@ import requests
 import unittest
 import technetium.bitbucket.bitchangesets as changesets
 
+
 class BitchangesetsTests(unittest.TestCase):
 
+    def setUp(self):
+        """
+        Set up API calls.
+        """
+        self.public_url = 'https://bitbucket.org/api/1.0/repositories/DrkSephy/smw-koopa-krisis/changesets/?limit=2'
+        self.private_url = 'https://bitbucket.org/api/1.0/repositories/DrkSephy/technetium/changesets/?limit=2'
     
     def test_get_public_changesets(self):
         """
         Tests that a public repository query has a 200 status code.
         """
-        req_url = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/smw-koopa-krisis/issues/?limit=2')
+        req_url = requests.get(self.public_url)
         self.assertEqual(req_url.status_code, 200)
 
     def test_get_private_changesets(self):
         """
         Tests that a private repository query without credentials will fail.
         """
-        req_url = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/techetium/issues/?limit=2')
+        req_url = requests.get(self.private_url)
         self.assertEqual(req_url.status_code, 404)
+
+   
+
+
+
+
 
 
 
