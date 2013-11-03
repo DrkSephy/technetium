8464e84403ddeb7beeaa928751e05f0e5eb5189e
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-02 22:04:26 -0400

    Adding functional tests

--- technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
+++ technetium/technetium/bitbucket/tests/functional_tests/test_changesets.py
@@ -8,5 +8,11 @@ import unittest
 import technetium.bitbucket.bitchangesets as changesets
 
 class BitchangesetsTests(unittest.TestCase):
-    pass
+    
+    def test_get_changesets(self):
+        """
+        Tests that a public repository query has a 200 status code.
+        """
+        req_url = requests.get('https://bitbucket.org/api/1.0/repositories/DrkSephy/smw-koopa-krisis/issues/?limit=2')
+        self.assertEqual(req_url.status_code, 200)
 
