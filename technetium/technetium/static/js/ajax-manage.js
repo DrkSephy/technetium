deeb11a561489c75132590e7c044830c9ff37fed
Jorge Yau <codenameyau@gmail.com>
2013-11-05 09:35:12 -0500

    Adding AJAX for subscribing to repository

--- technetium/technetium/static/js/ajax-manage.js
+++ technetium/technetium/static/js/ajax-manage.js
@@ -1,1 +1,7 @@
 // Put AJAX Requests for Manage in this File
+
+// AJAX Request to subscribe to repository
+$(":button").click(function() {
+    var button_id = this.id;
+    alert("Subscribing to " + button_id);
+})
