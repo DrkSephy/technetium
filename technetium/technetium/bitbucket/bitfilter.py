c1fb9ed92963a31309254611a8a707db3fa54e92
Albert Chieu <achieu00@citymail.cuny.edu>
2013-11-04 23:48:07 -0500

    implemented filter issues by status and fixed up some methods to enhance filtering module

--- technetium/technetium/bitbucket/bitfilter.py
+++ technetium/technetium/bitbucket/bitfilter.py
@@ -20,7 +20,9 @@ def filter_issues(request, parsed_json):
     filtered_json = parsed_json
     for n, v in request.GET.iteritems():
         if n.strip().lower() == 'type':
-            filtered_json = filter_issues_by_type(parsed_json, v)
+            filtered_json = filter_issues_by_type(filtered_json, v)
+        if n.strip().lower() == 'priority':
+            filtered_json = filter_issues_by_priority(filtered_json, v)
 
     return filtered_json
 
@@ -38,12 +40,17 @@ def filter_issues_by_type(parsed_json, filtered_value):
     return filtered_json
 
 
-def filter_issues_by_priority():
+def filter_issues_by_priority(parsed_json, filtered_value):
     """
     Filters issues based on priority.
     """
 
-    pass
+    filtered_json = []
+    for issue in parsed_json:
+        if issue['priority'] is not None and issue['priority'].strip().lower() == filtered_value.strip().lower():
+            filtered_json.append(issue)
+
+    return filtered_json
 
 
 def filter_issues_by_user():
--- technetium/technetium/bitbucket/bitissues.py
+++ technetium/technetium/bitbucket/bitissues.py
@@ -72,6 +72,15 @@ def parse_issues(request, raw_json):
 
         parsed_issues.append(data)
 
+    # filter issues based on query parameters
     parsed_issues = bitfilter.filter_issues(request, parsed_issues)
-    return parsed_issues
+    # limit 50 issues to be displayed
+    display_parsed_issues = []
+    count = 0
+    for issue in parsed_issues:
+        if count < 50:
+            display_parsed_issues.append(issue)
+            count += 1
+
+    return display_parsed_issues
 
