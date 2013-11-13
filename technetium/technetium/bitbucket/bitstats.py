379da220f03654b8c97da3cc60b779fdd39ebc60
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-12 22:38:28 -0500

    Working on commit counter

--- technetium/technetium/bitbucket/bitstats.py
+++ technetium/technetium/bitbucket/bitstats.py
@@ -5,3 +5,17 @@ Module for computing statistics for Bitbucket.
 import requests
 import simplejson as json
 
+def tally_changesets(data):
+    """
+    Counts the number of commits for each developer in a repository.
+
+    Parameters:
+    -----------
+    data: dictionary 
+
+    Returns:
+    --------
+    tally: dictionary
+    """
+
+    pass
