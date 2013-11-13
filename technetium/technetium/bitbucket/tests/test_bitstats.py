aa648488a73f5135af8749bd76f6908faa0d99f4
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-12 23:05:25 -0500

    Intermediate work on bitstats testing

--- technetium/technetium/bitbucket/tests/test_bitstats.py
+++ technetium/technetium/bitbucket/tests/test_bitstats.py
@@ -4,3 +4,13 @@ Test Technetium Bitbucket: bitstats
 
 import technetium.bitbucket.bitstats as bitstats
 import unittest
+
+class BitstatsTests(unittest.TestCase):
+
+    def setUp(self):
+        # Public user repository
+        self.pub_user = 'DrkSephy'
+        self.pub_repo = 'smw-koopa-krisis'
+        self.json = """
+        [{'author': 'Kevin Chan'}, {'author': 'DrkSephy'}]
+        """
