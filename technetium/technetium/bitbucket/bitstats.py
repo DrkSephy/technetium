0a8c57a451b4b7352311f608910f20761fa7fc55
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-12 22:43:49 -0500

    Finished commit counter method

--- technetium/technetium/bitbucket/bitstats.py
+++ technetium/technetium/bitbucket/bitstats.py
@@ -18,8 +18,19 @@ def tally_changesets(data):
     tally: dictionary
     """
 
+    # Dictionary to store commit counts
     tally = {}
+    # Iterate over all dictionaries in our list
     for i in data:
         for k,v in i.iteritems():
+            # If author is in dictionary, +=1.
+            # Otherwise, add the author to the dictionary 
+            # and start his counter to be 1.
             if v in tally:
-                
+                tally[v] += 1
+            else: 
+                tally[v] = 1
+
+    # Return a dictionary of the tally.
+    # Example: {DrkSephy: 9, Jorge Yau: 15}
+    return tally
