9480d33655fa26b4a29707d5ce4e39255d6d5c44
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-12 23:01:46 -0500

    Starting bitstats module testing

--- technetium/technetium/bitbucket/tests/test_bitstats.py
+++ technetium/technetium/bitbucket/tests/test_bitstats.py
@@ -1,29 +1,6 @@
 """
 Test Technetium Bitbucket: bitstats
 """
+
+import technetium.bitbucket.bitstats as bitstats
 import unittest
-import technetium.bitbucket.bitstats
-
-class BitstatsTest(unittest.TestCase):
-
-	def setUp(self):
-		# Public user repository
-		self.pub_user = 'DrkSephy'
-		self.pub_repo = 'smw-koopa-krisis'
-
-		# Private user repository
-		self.priv_user = 'technetiumccny'
-		self.priv_repo = 'technetium'
-
-    # These tests might need to be mocked
-	def test_get_diffstat_not_empty(self):
-		"""
-		Test that get changeset statistics 
-		"""
-		pass
-
-	def test_get_diff_not_empty(self):
-		"""
-		Test that get diffs 
-		"""
-		pass				
