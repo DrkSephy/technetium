9fbb22218981a6f19362f70d32c756c7ed782ff0
David Leonard <sephirothcloud1025@yahoo.com>
2013-11-30 17:00:27 -0500

    Cleaned up tests for changesets, finished testing parse_changesets

--- technetium/technetium/bitbucket/tests/test_bitchangesets.py
+++ technetium/technetium/bitbucket/tests/test_bitchangesets.py
@@ -16,39 +16,35 @@ class BitchangesetsTests(unittest.TestCase):
         self.repo = 'smw-koopa-krisis'
         self.endpoint = 'changesets'
         self.url = 'https://bitbucket.org/api/1.0/repositories/DrkSephy/smw-koopa-krisis/changesets/0/0'
-        self.limit = 0
-        self.start = 0
         self.auth_tokens = {}
         self.data3 = { "count": 532, "start": "0", "limit": 0, "changesets": []}
-
-
-        # Private user repository
-        self.priv_user = 'technetiumccny'
-        self.priv_repo = 'technetium'
-        self.data = [{'utctimestamp': '2013-11-11 03:14:50+00:00', 'author': 'DrkSephy', 
-                    'timestamp': '2013-11-11 04:14:50', 'raw_author': 'David Leonard <sephirothcloud1025@yahoo.com>'}]
-        self.data2 = [{'utctimestamp': '2013-11-11 03:14:50+00:00', 'author': 'DrkSephy', 
-                    'timestamp': '2013-11-11 04:14:50', 'raw_author': 'David Leonard'}]
-
+        self.changeset = {
+              "raw_author": "David Leonard <sephirothcloud1025@yahoo.com>",
+              "timestamp": "2013-07-27 01:56:46",
+        }
+        self.changeset2 = {
+              "raw_author": "David Leonard",
+              "timestamp": "2013-07-27 01:56:46",
+        }
+        
 
     def test_get_parse_not_empty(self):
         """
         Tests that parsing changesets returns a non-empty list.
         """
-        pass
+        
+        self.assertEqual(bitchangesets.parse_changeset(self.changeset), {'timestamp': '2013-07-27 01:56:46', 'parsed_author': 'David Leonard'}) 
 
-    def test_parse_changesets_empty(self):
-        """
-        Tests that parsing an empty changeset list returns empty list.
-        """
-        pass
     def test_parse_changesets_emails(self):
         """
         Tests that parsing a changeset list with user emails returns a properly parsed list.
         """
-        pass
+        
+        self.assertEqual(bitchangesets.parse_changeset(self.changeset), {'timestamp': '2013-07-27 01:56:46', 'parsed_author': 'David Leonard'}) 
+
     def test_parse_changesets_no_emails(self):
         """
         Tests that parsing a changeset list without user emails returns a properly parsed list.
         """
-        pass
+        
+        self.assertEqual(bitchangesets.parse_changeset(self.changeset2), {'timestamp': '2013-07-27 01:56:46', 'parsed_author': 'David Leonard'}) 
