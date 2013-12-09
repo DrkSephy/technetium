6d40a22bbb3daa3ab4176194519a521272da9c71
Jorge Yau <codenameyau@gmail.com>
2013-12-08 21:49:37 -0500

    Refactoring screenshot code for DRY

--- technetium/technetium/bitbucket/bitdashboard.py
+++ technetium/technetium/bitbucket/bitdashboard.py
@@ -23,7 +23,7 @@ def get_screenshots(num_screenshots=5):
     screenshots = []
     for i in range(num_screenshots):
         data = {}
-        data['screenshot-id'] = i
+        data['id'] = i
         data['filename'] = "/static/img/screenshots/screenshot-%d.jpg" % i
         data['description'] = descriptions[i]
         screenshots.append(data)
